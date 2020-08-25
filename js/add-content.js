d5ee96371077bd312298118aa757a1457f4daea1
Hamza-Rashed <hamzarashed503@gmail.com>
2020-08-25 22:24:35 +0300

    new calucaletr

---
+++ js/add-content.js
@@ -0,0 +1,33 @@
+let number1 = prompt('Enter the first Number')
+let number2 = prompt('Enter the second number')
+let operation = prompt('Enter the operation')
+
+let getting;
+
+    if (operation == '') {
+        getting = 'the operation is Empty'
+    } 
+    else if (operation == '+') {
+
+        getting = Number(number1) + Number(number2)
+    } 
+    else if (operation == '-') {
+
+        getting = number1 - number2
+    } 
+    else if (operation == '*') {
+
+        getting = number1 * number2
+    } 
+    else if (operation == '/') {
+
+        getting = number1 / number2
+    } 
+    else {
+
+getting = `This operation <span>  ${operation}  </span> does not exist`
+
+    }
+
+
+document.write('<h3>' + getting + '</h3>')
